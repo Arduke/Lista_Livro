5878509bf6640caa18e7a6408db84845c13893ba
Arduke <carlos.edumart@outlook.com>
2018-08-30 21:39:11 -0300

    1º commit

---
+++ projLivrosLista/Livro.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projLivrosLista
+{
+    class Livro
+    {
+       
+        #region atributos
+        private int isbn;
+        private string titulo;
+        private string autor;
+        private string editora;
+        private List<Exemplar> exemplares;
+        #endregion
+                
+        #region propriedades
+        public int Isbn
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        public string Editora
+        {
+            get { return editora; }
+            set { editora = value; }
+        }
+        #endregion
+
+        #region construtores
+        //contrutores
+        public Livro (int isbn, string titulo, string autor, string editora)
+        {
+            this.isbn = isbn;
+            this.titulo = titulo;
+            this.editora = editora;
+            this.autor = autor;
+        }
+        public Livro(int isbn)
+        {
+            this.isbn = isbn;
+        }
+        public Livro()
+            : this(0, "", "","")
+        {
+        }
+        #endregion
+              
+        #region metodos
+        public void adicionarExemplar(Exemplar exemplar)
+        {
+            this.exemplares.Add(exemplar);
+        }
+        public int qtdeExemplares()
+        {
+            return 0;
+        }
+        public int qtdeDisponiveis()
+        {
+            return 0;
+        }
+        public int qtdeEmprestimos()
+        {
+            return 0;
+        }
+        public double percDisponibilidade()
+        {
+            return 0;
+        }
+        #endregion
+
+        #region Sobreescritas
+        public override bool Equals(object obj)
+        {
+            Livro p = (Livro)obj;
+            return this.isbn.Equals(p.isbn);
+        }
+        #endregion
+    }
+}
---
+++ projLivrosLista/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projLivrosLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resposta;
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("|0 - Sair                        |");
+            Console.WriteLine("|1 - Adicionar Livro             |");
+            Console.WriteLine("|2 - Pesquisar Livro (sintético) |");
+            Console.WriteLine("|3 - Pesquisar Livro (analítico) |");
+            Console.WriteLine("|4 - Adicionar exemplar          |");
+            Console.WriteLine("|5 - Registrar empréstimo        |");
+            Console.WriteLine("|6 - Registrar devolução         |");
+            Console.WriteLine("----------------------------------");
+            resposta = Convert.ToInt32(Console.ReadLine());
+
+            switch (resposta)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
